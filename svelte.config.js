00e5470faa6b84ea50086ba24fe703eb9d981eeb
Naptie <naptie@qq.com>
2025-06-27 23:42:18 +0800

    chore: update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from '@sveltejs/adapter-auto';
 import cfAdapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+console.log('Adapter:', process.env.ADAPTER);
 const isCloudflare = process.env.ADAPTER === 'cloudflare';
 
 const config = {
