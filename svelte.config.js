d488e38fc0b8165bf9e47f88edb76c994b8a3879
Naptie <naptie@qq.com>
2025-06-27 23:17:43 +0800

    chore: add explicit cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
+import cfAdapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const isCloudflare = process.env.ADAPTER === 'cloudflare';
 
 const config = {
   preprocess: vitePreprocess(),
-  kit: { adapter: adapter() }
+  kit: { adapter: isCloudflare ? cfAdapter() : adapter() }
 };
 
 export default config;
