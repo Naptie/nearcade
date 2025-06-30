e1313da7334eb6f8c0beb06e111518075f825dce
Naptie <naptie@qq.com>
2025-07-01 00:46:12 +0800

    refactor(ui/SiteTitle): set user-select to none

--- src/lib/components/SiteTitle.svelte
+++ src/lib/components/SiteTitle.svelte
@@ -2,7 +2,7 @@
   let { class: klass = '' } = $props();
 </script>
 
-<a class="font-bold tracking-tight {klass}" href="/">
+<a class="font-bold tracking-tight select-none {klass}" href="/">
   <span
     class="from-primary dark:via-secondary to-secondary dark:to-accent bg-gradient-to-br bg-clip-text pr-[0.03em] text-transparent"
   >
