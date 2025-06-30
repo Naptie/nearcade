16f09c85784e54c919089afc74a2d77d0873b099
Naptie <naptie@qq.com>
2025-07-01 00:43:17 +0800

    refactor(ui/SiteTitle): use the em unit for padding right

--- src/lib/components/SiteTitle.svelte
+++ src/lib/components/SiteTitle.svelte
@@ -1,33 +1,10 @@
 <script lang="ts">
   let { class: klass = '' } = $props();
-  let element: HTMLSpanElement;
-  let paddingRight = $state(0);
-
-  const updatePadding = () => {
-    if (element) {
-      const width = element.offsetWidth;
-      paddingRight = width * 0.01029;
-    }
-  };
-
-  $effect(() => {
-    updatePadding();
-
-    window.addEventListener('resize', updatePadding);
-    window.addEventListener('orientationchange', updatePadding);
-
-    return () => {
-      window.removeEventListener('resize', updatePadding);
-      window.removeEventListener('orientationchange', updatePadding);
-    };
-  });
 </script>
 
 <a class="font-bold tracking-tight {klass}" href="/">
   <span
-    bind:this={element}
-    class="from-primary dark:via-secondary to-secondary dark:to-accent bg-gradient-to-br bg-clip-text text-transparent"
-    style="padding-right: {paddingRight}px"
+    class="from-primary dark:via-secondary to-secondary dark:to-accent bg-gradient-to-br bg-clip-text pr-[0.03em] text-transparent"
   >
     near
   </span><span class="text-base-content">cade</span>
