9b0bab1e91b3fb91dbdcb8a67d7a9b7753a0be3c
Naptie <naptie@qq.com>
2025-07-13 15:01:50 +0800

    feat: add /api/dicover endpoint

--- src/routes/(main)/discover/+page.ts
+++ src/routes/(main)/discover/+page.ts
@@ -1,6 +1,15 @@
+import { env } from '$env/dynamic/public';
+import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ data }) => {
+export const load: PageLoad = async ({ data, url, fetch }) => {
+  if (env.PUBLIC_API_BASE) {
+    const resp = await fetch(`${env.PUBLIC_API_BASE}/api/discover${url.search}`);
+    if (!resp.ok) {
+      error(resp.status, `Failed to fetch shops: ${resp.statusText}`);
+    }
+    data = await resp.json();
+  }
   const { shops, location, radius } = data;
 
   return {
---
+++ src/routes/api/discover/+server.ts
@@ -0,0 +1,13 @@
+import { loadShops } from '$lib/discover.server';
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ url }) => {
+  try {
+    const resp = await loadShops({ url });
+    return json(resp);
+  } catch (err) {
+    console.error('Error getting shops:', err);
+    throw error(500, 'Failed to get nearby shops');
+  }
+};
