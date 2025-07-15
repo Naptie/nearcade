2ef0739e694f91fd0f13241a9891bad6d63afacc
Naptie <naptie@qq.com>
2025-07-16 00:22:47 +0800

    feat: add Netlify adapter and configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 import adapterAuto from '@sveltejs/adapter-auto';
 import adapterStatic from '@sveltejs/adapter-static';
+import adapterNetlify from '@sveltejs/adapter-netlify';
 import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const isCloudflare = process.env.ADAPTER === 'cloudflare';
+const isNetlify = process.env.ADAPTER === 'netlify';
 const hasApiBase = process.env.PUBLIC_API_BASE && process.env.PUBLIC_API_BASE.trim() !== '';
 const base = process.env.PATH_BASE || '';
 
@@ -12,15 +14,19 @@ const config = {
   kit: {
     adapter: isCloudflare
       ? adapterCloudflare()
-      : hasApiBase
-        ? adapterStatic({
-            pages: 'build',
-            assets: 'build',
-            fallback: 'index.html',
-            precompress: false,
-            strict: true
+      : isNetlify
+        ? adapterNetlify({
+            edge: true
           })
-        : adapterAuto(),
+        : hasApiBase
+          ? adapterStatic({
+              pages: 'build',
+              assets: 'build',
+              fallback: 'index.html',
+              precompress: false,
+              strict: true
+            })
+          : adapterAuto(),
     paths: {
       base
     },
