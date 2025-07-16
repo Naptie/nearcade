862032a3819970a6ca2e786f5f8d693b09d3eaa7
Naptie <naptie@qq.com>
2025-07-16 16:29:20 +0800

    chore: revert netlify adaptation

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
 import adapterAuto from '@sveltejs/adapter-auto';
 import adapterStatic from '@sveltejs/adapter-static';
-import adapterNetlify from '@sveltejs/adapter-netlify';
 import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const isCloudflare = process.env.ADAPTER === 'cloudflare';
-const isNetlify = process.env.ADAPTER === 'netlify';
 const hasApiBase = process.env.PUBLIC_API_BASE && process.env.PUBLIC_API_BASE.trim() !== '';
 const base = process.env.PATH_BASE || '';
 
@@ -14,20 +12,15 @@ const config = {
   kit: {
     adapter: isCloudflare
       ? adapterCloudflare()
-      : isNetlify
-        ? adapterNetlify({
-            edge: false,
-            split: true
+      : hasApiBase
+        ? adapterStatic({
+            pages: 'build',
+            assets: 'build',
+            fallback: 'index.html',
+            precompress: false,
+            strict: true
           })
-        : hasApiBase
-          ? adapterStatic({
-              pages: 'build',
-              assets: 'build',
-              fallback: 'index.html',
-              precompress: false,
-              strict: true
-            })
-          : adapterAuto(),
+        : adapterAuto(),
     paths: {
       base
     },
