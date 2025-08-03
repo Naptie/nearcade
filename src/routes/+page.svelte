6311dac22712c45f7d4544fd2dacb9a6b2a18307
Naptie <naptie@qq.com>
2025-08-04 00:42:38 +0800

    fix: improve button layout for university and club links on the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -279,18 +279,17 @@
         <div class="join">
           <a
             href="{base}/universities"
-            class="btn btn-soft hover:bg-primary join-item hover:text-primary-content gap-2 py-5 sm:px-6 dark:hover:bg-white dark:hover:text-black"
+            class="btn btn-soft hover:bg-primary join-item hover:text-primary-content flex-1 gap-2 py-5 text-nowrap sm:px-6 dark:hover:bg-white dark:hover:text-black"
           >
             {m.find_university()}
             <i class="fa-solid fa-graduation-cap fa-lg"></i>
           </a>
-          <a href="{base}/clubs">
-            <button
-              class="btn btn-soft hover:bg-primary join-item hover:text-primary-content gap-2 py-5 sm:px-6 dark:hover:bg-white dark:hover:text-black"
-            >
-              {m.find_clubs()}
-              <i class="fa-solid fa-users fa-lg"></i>
-            </button>
+          <a
+            href="{base}/clubs"
+            class="btn btn-soft hover:bg-primary join-item hover:text-primary-content flex-1 gap-2 py-5 text-nowrap sm:px-6 dark:hover:bg-white dark:hover:text-black"
+          >
+            {m.find_clubs()}
+            <i class="fa-solid fa-users fa-lg"></i>
           </a>
         </div>
       </div>
