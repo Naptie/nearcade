90b59f8abdfc95bfd5c5401c8d4cc53e471d7d8d
Naptie <naptie@qq.com>
2025-08-16 03:00:14 +0800

    chore: explicitly add Vercel adapter support in Svelte configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import adapterAuto from '@sveltejs/adapter-auto';
 import adapterNode from '@sveltejs/adapter-node';
 import adapterStatic from '@sveltejs/adapter-static';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import adapterCloudflare from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const isNode = process.env.ADAPTER === 'node';
+const isVercel = process.env.ADAPTER === 'vercel';
 const isCloudflare = process.env.ADAPTER === 'cloudflare';
 const hasApiBase = process.env.PUBLIC_API_BASE && process.env.PUBLIC_API_BASE.trim() !== '';
 const base = process.env.PATH_BASE || '';
@@ -14,17 +16,19 @@ const config = {
   kit: {
     adapter: isNode
       ? adapterNode()
-      : isCloudflare
-        ? adapterCloudflare()
-        : hasApiBase
-          ? adapterStatic({
-              pages: 'build',
-              assets: 'build',
-              fallback: 'index.html',
-              precompress: false,
-              strict: true
-            })
-          : adapterAuto(),
+      : isVercel
+        ? adapterVercel()
+        : isCloudflare
+          ? adapterCloudflare()
+          : hasApiBase
+            ? adapterStatic({
+                pages: 'build',
+                assets: 'build',
+                fallback: 'index.html',
+                precompress: false,
+                strict: true
+              })
+            : adapterAuto(),
     paths: {
       base
     },
