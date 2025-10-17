a1812b54d8fdcd49000ffa34643c8d2acc3281c5
Naptie <naptie@qq.com>
2025-10-17 21:03:54 +0800

    fix: disable eslint rule for at-html-tags in clubs and universities pages

--- src/routes/(main)/clubs/+page.svelte
+++ src/routes/(main)/clubs/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  /* eslint svelte/no-at-html-tags: "off" */
   import { m } from '$lib/paraglide/messages';
   import { goto } from '$app/navigation';
   import { page } from '$app/state';
