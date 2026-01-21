e02dd980d95a46638c94b571332351aca7338c97
Naptie <naptie@qq.com>
2026-01-21 19:08:10 +0800

    feat: add support for WeChat template messages and automatic token refresh

--- src/lib/utils/index.server.ts
+++ src/lib/utils/index.server.ts
@@ -2,6 +2,7 @@ import type { ShopSource } from '$lib/constants';
 import mongo from '$lib/db/index.server';
 import redis, { ensureConnected } from '$lib/db/redis.server';
 import type { Shop } from '$lib/types';
+import { ObjectId } from 'mongodb';
 
 export const getCurrentAttendance = async (userId: string) => {
   const attendancePattern = `nearcade:attend:*:${userId}:*`;
@@ -24,4 +25,60 @@ export const getCurrentAttendance = async (userId: string) => {
     }
   }
   return null;
+};
+
+export const sendWeChatTemplateMessage = async (
+  userId: string | undefined,
+  templateId: string,
+  data: Record<string, string>,
+  url?: string
+) => {
+  const db = mongo.db();
+  const accountsCollection = db.collection('accounts');
+  const userAccount = await accountsCollection.findOne({
+    userId: new ObjectId(userId),
+    provider: 'wechat'
+  });
+  if (userAccount && userAccount.providerAccountId) {
+    const openId = userAccount.providerAccountId;
+
+    await ensureConnected();
+    const accessToken = await redis.get('nearcade:wechat:access_token');
+
+    if (!accessToken) {
+      console.error('WeChat access token not found in Redis');
+      return;
+    }
+
+    const formattedData: Record<string, { value: string }> = {};
+    for (const [key, value] of Object.entries(data)) {
+      formattedData[key] = { value };
+    }
+
+    const body = {
+      touser: openId,
+      template_id: templateId,
+      url,
+      data: formattedData
+    };
+
+    console.log(body);
+
+    try {
+      const response = await fetch(
+        `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${accessToken}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(body)
+        }
+      );
+      const result = await response.json();
+      console.log('WeChat template message result:', result);
+    } catch (error) {
+      console.error('Error sending WeChat template message:', error);
+    }
+  }
 };
