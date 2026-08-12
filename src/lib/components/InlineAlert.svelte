36b9e1e610c2a5079d4b1755e07ee622bfe264a3
Naptie <naptie@qq.com>
2026-08-12 12:39:26 +0800

    fix: correct CSS class for word wrapping in InlineAlert component

--- src/lib/components/InlineAlert.svelte
+++ src/lib/components/InlineAlert.svelte
@@ -33,6 +33,6 @@
       </div>
     </div>
   {:else}
-    <div class="min-w-0 flex-1 break-words">{@render children?.()}</div>
+    <div class="min-w-0 flex-1 wrap-break-word">{@render children?.()}</div>
   {/if}
 </div>
